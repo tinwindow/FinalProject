c54e85045b23fea8dd673cf05a9d0bdb233f41ab
tinwindow <erin.ura@gmail.com>
2017-08-29 14:10:03 -0400

    images and description working

--- public/js/remedycontroller.js
+++ public/js/remedycontroller.js
@@ -1,23 +1,26 @@
 var app = angular.module("remedyApp");
 
   app.controller("remedyController", function($scope, $http, $location){
-      var ailmenttitle = "Cramps";
-      //var $scope.ailmentId = {};
-    $http({
-      method: 'GET',
-      url: "/remedies",
-      params: {
-        ailment: 'Migraine',
-        test: 'jjytjy'
-      }
-    }).then(function(response) {
-            console.log(response.data);
-            $scope.remedy = response.data;
-        })
 
     var ailmentName = $location.search().ailment;
     console.log(ailmentName);
 
+    $scope.title = ailmentName; 
 
-    //  $scope.selectedRemedy = remedyService.getRemedy();
+    $http({
+      method: 'GET',
+      url: "/remedies",
+      params: {
+        ailment: ailmentName
+
+      }
+    }).then(function success (response) {
+            console.log(response.data);
+            $scope.remedies = response.data;
+        })
+
+
+
+
+
   })
